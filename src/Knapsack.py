19fd05cadfcecb4394ddf82a2760006ae42f9438
DarylMartinDipp <didieravangers@gmail.com>
2021-12-27 16:32:05 +0100

    Optimization & comments
    New methods modified and implemented

--- src/Knapsack.py
+++ src/Knapsack.py
@@ -1,9 +1,21 @@
-def get_value_content(objects_dict, self_content):
-    return objects_dict.get(self_content)[0]
+def get_value(objects_dict, item) -> int:
+    """
+    Get the value of the item in the dictionary used
+    :param objects_dict: the dictionary used
+    :param item: the name of the item we want the value
+    :return: int: the value of the item
+    """
+    return objects_dict.get(item)[0]
 
 
-def get_weight_content(objects_dict, self_content):
-    return objects_dict.get(self_content)[1]
+def get_weight(objects_dict, item) -> int:
+    """
+    Get the weight of the item in the dictionary used
+    :param objects_dict: the dictionary used
+    :param item: the name of the item we want the weight
+    :return: int: the weight of the item
+    """
+    return objects_dict.get(item)[1]
 
 
 class Knapsack:
@@ -13,40 +25,53 @@ class Knapsack:
 
     def get_value_and_weight(self, objects_dict) -> (int, int):
         """
-        Return the value and the weight of a knapsack
+        Get the value and the weight of a knapsack in the dictionary used
         :param objects_dict: the dictionary used
-        :return: the value & the weight
+        :return: (int,int): the value & the weight
         """
         value = 0
         weight = 0
         for self_content in self.content:
-            value += get_value_content(objects_dict, self_content)
-            weight += get_weight_content(objects_dict, self_content)
+            value += get_value(objects_dict, self_content)
+            weight += get_weight(objects_dict, self_content)
         return value, weight
 
     def print_content(self, objects_dict) -> None:
         """
-        Print the content of the knapsack using the to_string method
+        Print the content of the knapsack using the two to_string methods
         :param objects_dict: the dictionary used
         """
-        print(self.to_string(objects_dict))
+        print(self.items_to_string(objects_dict),
+              self.info_to_string(objects_dict),
+              sep='')
 
-    def to_string(self, objects_dict):
+    def items_to_string(self, objects_dict) -> str:
         """
-        Return the content of knapsack as a string
+        Get the name, the value and the weight of the content of the knapsack as a string using the dictionary
         :param objects_dict: the dictionary used
-        :return: the content of knapsack as a string
+        :return: str: the name, the value and the weight of the content of the knapsack as a string
         """
-        string = ''
-        for self_content in self.content:
-            string += self_content + ' '
-            string += get_value_content(objects_dict, self_content).__str__() + ' '
-            string += get_weight_content(objects_dict, self_content).__str__() + '\n'
+        items_string = ''
+        for item in self.content:
+            items_string += item + ' '
+            items_string += objects_dict.get(item)[0].__str__() + ' '
+            items_string += objects_dict.get(item)[1].__str__() + '\n'
+        return items_string
 
-        string += 'Le sac a ' + len(self.content).__str__() + ' objets, '
-        string += 'pour une valeur de ' + str(self.get_value_and_weight(objects_dict)[0])
-        string += ' et un poids de ' + str(self.get_value_and_weight(objects_dict)[1]) + '/' + str(self.capacity)
-        return string
+    def info_to_string(self, objects_dict) -> str:
+        """
+        Get the information of the knapsack as a string using the dictionary
+        :param objects_dict: the dictionary used
+        :return: str: the information of the knapsack as a string
+        """
+        knapsack_value_and_weight = self.get_value_and_weight(objects_dict)
+        info_string = 'Le sac a ' + len(self.content).__str__()
+        info_string += ' objets, pour une valeur de '
+        info_string += knapsack_value_and_weight[0].__str__()
+        info_string += ' et un poids de '
+        info_string += knapsack_value_and_weight[1].__str__()
+        info_string += '/' + self.capacity.__str__()
+        return info_string
 
 
 def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
@@ -54,31 +79,43 @@ def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
     Resolve the knapsack problem in the greedy way :
     sort the objects according to their values per weight, and add gradually to the bag the
     elements having the greatest value per weight, until the moment when the bag is full.
-
     :param knapsack: the knapsack to sort
     :param objects_dict: the dictionary used
-    :return: the knapsack sorted
+    :return: Knapsack: the knapsack sorted
     """
-    # Make a dictionary which associate objects names by objects value by weight
-    dict_value_by_weight = {}
-    for name in objects_dict.keys():
-        dict_value_by_weight[name] = objects_dict[name][0] / objects_dict[name][1]
-
-    # Sort it
-    sorted_dict = sorted(dict_value_by_weight.items(), key=lambda dict_content: dict_content[1], reverse=True)
+    # Fill the knapsack with objects which have the best value per weight
+    sorted_greedy_list = get_sorted_greedy_list(objects_dict)
 
     # Keep adding content to the knapsack until no more content can be added
-    knapsack_weight = 0
+    knapsack_weight = knapsack.get_value_and_weight(objects_dict)[1]
 
-    for name, weight_by_value in sorted_dict:
-        # Stop looping if the knapsack is already full
-        if knapsack_weight == knapsack.capacity:
-            break
+    for item_name, useless in sorted_greedy_list:
         # Check if the next content will fit in the knapsack
-        if knapsack_weight + objects_dict[name][1] <= knapsack.capacity:
-            knapsack_weight += objects_dict[name][1]
-            knapsack.content.append(name)
+        if knapsack_weight + objects_dict[item_name][1] <= knapsack.capacity:
+            knapsack_weight += objects_dict[item_name][1]
+            knapsack.content.append(item_name)
+
+            # Stop looping if the knapsack is full
+            if knapsack_weight == knapsack.capacity:
+                break
     return knapsack
+
+
+def get_sorted_greedy_list(objects_dict) -> list:
+    """
+    Create a list associating the name of each item in the dictionary by its value per weight
+    :param objects_dict: the dictionary used
+    :return: list: list with the name of each item in the dictionary and its value per weight
+    """
+    # Make a dictionary which associate objects names by its value per weight
+    greedy_dict = {}
+    for item in objects_dict.keys():
+        greedy_dict[item] = objects_dict[item][0] / objects_dict[item][1]
+    # Make a sorted list by value per weight with the greedy_dict content
+    sorted_greedy_list = sorted(greedy_dict.items(),
+                                key=lambda content: content[1],
+                                reverse=True)
+    return sorted_greedy_list
 
 
 def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
