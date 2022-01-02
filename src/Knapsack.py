280d821289fb08a40f05cffd3f9eeeb1a8f689a7
DarylMartinDipp <didieravangers@gmail.com>
2022-01-02 18:22:35 +0100

    Last improvements and comments
    Optimal is over

--- src/Knapsack.py
+++ src/Knapsack.py
@@ -2,7 +2,7 @@ def get_value(objects_dict, item) -> int:
     """
     Get the value of the item in the dictionary used
     :param objects_dict: the dictionary used
-    :param item: the name of the item we want the value
+    :param item: the name of the item whose value is desired
     :return: int: the value of the item
     """
     return objects_dict.get(item)[0]
@@ -12,7 +12,7 @@ def get_weight(objects_dict, item) -> int:
     """
     Get the weight of the item in the dictionary used
     :param objects_dict: the dictionary used
-    :param item: the name of the item we want the weight
+    :param item: the name of the item whose weight is desired
     :return: int: the weight of the item
     """
     return objects_dict.get(item)[1]
@@ -20,6 +20,10 @@ def get_weight(objects_dict, item) -> int:
 
 class Knapsack:
     def __init__(self, capacity):
+        """
+        Initialize the knapsack
+        :param capacity: the knapsack's capacity
+        """
         self.capacity = capacity
         self.content = []
 
@@ -122,59 +126,93 @@ def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
 
 
 def solve_knapsack_optimal(knapsack, objects_dict) -> Knapsack:
+    """
+    Resolve the knapsack problem in the optimal way :
+    give the best solution, searching among all the possible solutions.
+    :param knapsack: the knapsack to sort
+    :param objects_dict: the dictionary used
+    :return: Knapsack: the knapsack sorted
+    """
     index_weight = knapsack.capacity
-    # si le sac a une capacité = 0 on le retourne directement
+
+    # If the knapsack has zero capacity, return the empty knapsack
     if index_weight == 0:
         return knapsack
-    index_items = len(objects_dict)
-    # transforme le dictionnaire en liste
+
+    # Transform the dictionary into a list
     objects_list = list(objects_dict.items())
-    # initialise la matrice (1 ligne par item et 1 colonne par poids (avec 0))
-    matrice = get_dynamic_program_matrice(index_weight, index_items, objects_list)
+    index_items = len(objects_dict)
 
-    # afin d'eviter d'avoir une erreur d'index
+    # Build a matrix with the get_dynamic_program_matrix method
+    # 1 row per item & 1 column per unit of knapsack's weight
+    matrix = get_dynamic_program_matrix(index_weight, index_items, objects_list)
+
+    # Fill the knapsack by using the matrix :
+
+    # If the last value of the last item's row is the same of the before last value,
+    # the value does not need to be taken, so it pass to the previous one by decreasing
     index_items -= 1
-
-    while matrice[index_items][index_weight] == matrice[index_items][index_weight - 1]:
+    while matrix[index_items][index_weight] == matrix[index_items][index_weight - 1]:
         index_weight -= 1
 
     while index_weight > 0:
-        while index_items > 0 and matrice[index_items][index_weight] == matrice[index_items - 1][index_weight]:
+        # While the last item have the same value of the before last item for the same knapsack's weight,
+        # the item does not need to be taken, so it pass to the previous one by decreasing
+        while index_items > 0 and matrix[index_items][index_weight] == matrix[index_items - 1][index_weight]:
             index_items -= 1
-        index_weight = index_weight - objects_list[index_items][1][1]
+        index_weight -= objects_list[index_items][1][1]
         if index_weight >= 0:
+            # Adding the item in the knapsack
             knapsack.content.append(objects_list[index_items][0])
         index_items -= 1
     return knapsack
 
 
-def get_dynamic_program_matrice(weight, data_size, data_list):
-    matrice = [[0 for _ in range(weight + 1)] for _ in range(data_size)]
-    # programmation dynamique
-    # pour le premier objet (pb d'index sinon)
+def get_dynamic_program_matrix(weight, data_size, data_list):
+    """
+    Build a matrix with 1 row per dictionary's item & 1 column per unit of knapsack's weight
+    :param weight: the knapsack's capacity
+    :param data_size: the length of the dictionary
+    :param data_list: the dictionary as a list
+    :return: the matrix created
+    """
+    # Initialization of the matrix
+    matrix = [[0 for _ in range(weight + 1)] for _ in range(data_size)]
+
+    # Matrix filling :
+
+    # One loop for the first item
     for i in range(0, weight + 1):
-        # [0] objet 0, [1] tableau, [1] poids
-        # si le poids de la colonne est assez grand pour contenir l'objet
-        if not data_list[0][1][1] > i:
-            matrice[0][i] = data_list[0][1][0]
-    # pour le reste des objets
-    for i in range(1, data_size):  # fait le tour des objets
-        for j in range(0, weight + 1):  # fait le tour des poids
-            if data_list[i][1][1] > j:
-                matrice[i][j] = matrice[i - 1][j]
-            else:  # on prend le max entre la ligne du dessus a la même colonne et la valeur optimisé a la ligne du dessus pour un poids pouvant contenir le dernier objet
-                matrice[i][j] = max(matrice[i - 1][j], matrice[i - 1][j - data_list[i][1][1]] + data_list[i][1][0])
-    return matrice
+        item_value, item_weight = data_list[0][1]
+        # If the column is "heavy" enough to contain the weight of the first item
+        if item_weight <= i:
+            # The value is put in the matrix, at the right column
+            matrix[0][i] = item_value
+
+    # One loop for the other items
+    for i in range(1, data_size):
+        # One loop per unit of weight + 1
+        for j in range(0, weight + 1):
+            # If the column is not "heavy" enough to contain the weight of the actual item
+            item_value, item_weight = data_list[i][1]
+            if item_weight > j:
+                # The value of the last item at the same weight's column is put
+                matrix[i][j] = matrix[i - 1][j]
+            else:
+                # The maximum value between the last item at the same weight's column
+                # the value in the previous line at weight's column which can contain the actual item
+                matrix[i][j] = max(matrix[i - 1][j], matrix[i - 1][j - item_weight] + item_value)
+    return matrix
 
 
 if __name__ == '__main__':
-    # dico = {
-    #     'Objet0': [15, 5],
-    #     'Objet1': [10, 1],
-    #     'Objet2': [12, 3],
-    #     'Objet3': [4, 17],
-    #     'Objet4': [20, 4],
-    #     'Objet5': [2, 1],
-    # }
-    # sac = Knapsack(5)
-    # solve_knapsack_optimal(sac, dico).print_content(dico)
+    dico = {
+        'Objet0': [15, 5],
+        'Objet1': [10, 1],
+        'Objet2': [12, 3],
+        'Objet3': [4, 17],
+        'Objet4': [20, 4],
+        'Objet5': [2, 1],
+    }
+    sac = Knapsack(5)
+    solve_knapsack_optimal(sac, dico).print_content(dico)
